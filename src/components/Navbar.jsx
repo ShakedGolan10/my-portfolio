735664a73131efa84992f67b2d6fb64d678b6294
Shaked Golan <shaked518@gmail.com>
2023-12-28 06:45:01 +0200

    fixing padding at nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`w-full flex item-center py-5 fixed
+      className={`w-full flex item-center py-5 px-5 fixed
      top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to='/' className='flex items-center gap-2'
