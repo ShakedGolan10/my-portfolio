1e37c136daa5e3f953241310853e0b767781032b
Shaked Golan <shaked518@gmail.com>
2024-01-03 01:52:49 +0200

    fixing a link

--- src/constants/index.js
+++ src/constants/index.js
@@ -239,7 +239,7 @@ const projects = [
     source_demo_link: "https://github.com/ShakedGolan10/shiftable-app",
   },
   {
-    name: "Instegram Clone",
+    name: "Picgram",
     description:
       "A social media app as Instagram, where you can share your photos, like posts , save posts , search for users on the explore and watch people profiles. also upload and watch stories!.",
     tags: [
@@ -270,7 +270,7 @@ const projects = [
     ],
     image: snapgram,
     source_code_link: "https://github.com/YuvalDavidov/instagram-clone",
-    source_demo_link: "https://instagram-clone-7ir9.onrender.com",
+    source_demo_link: "https://picgram-app.onrender.com/",
   },
   {
     name: "Airtnt",
